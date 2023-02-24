8f62e00ff363d5a3f5d3e0e1dbf98553a7f1654a
nbradley7407 <121584105+nbradley7407@users.noreply.github.com>
2023-02-24 13:45:13 -0700

    refactoring, improvements

--- coverage_scrape.py
+++ coverage_scrape.py
@@ -1,7 +1,3 @@
-import nltk
-from newspaper import Article
-from newspaper import ArticleException
-import time
 from selenium import webdriver
 from selenium.common.exceptions import StaleElementReferenceException
 from selenium.webdriver.chrome.service import Service
@@ -37,35 +33,22 @@ my_tickers = [
     "RBLX",
 ]
 
-
+# initiate driver
 service = Service('chromedriver.exe')
 service.start()
 driver = webdriver.Remote(service.service_url)
 wait = WebDriverWait(driver, 60.0)
 
 
-def get_info(article):
-    a = Article(article)
-    try:
-        a.download()
-        a.parse()
-        a.nlp()
-        return f'Title: {a.title}',\
-               f'Date: {a.publish_date}'
-    except ArticleException:
-        print(f"Error getting summary")
-
-
 def get_title(element):
-
     title_element = element.find_element(By.XPATH, "//*[@id=\"rso\"]/div/div/div[1]/div/div/a/div/div[2]/div[2]")
     title = title_element.get_attribute("innerHTML")
     return title
 
 
 def get_url(element):
-    url = element.get_attribute('href')
-    return url
+    url_string = element.get_attribute('href')
+    return url_string
 
 
 def get_date(element):
@@ -74,29 +57,32 @@ def get_date(element):
     return date
 
 
-def scrape(ticker):
-    driver.get(f'https://www.google.com/search?q={ticker}+stock&hl=en&tbm'
-               f'=nws&source=lnt&tbs=sbd:1&sa=X&ved=2ahUKEwitoYTL16n9AhVNP'
-               f'n0KHbyMBE4QpwV6BAgBECE&biw=2067&bih=2007&dpr=1')
+def scrape_info():
+    wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "iRPxbe")))
+    article_info = driver.find_elements(By.CLASS_NAME, "iRPxbe")
+    return article_info[:3]
+
+def scrape_urls():
     wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "WlydOe")))
-    article_elements = driver.find_elements(By.CLASS_NAME, "WlydOe")
-    return article_elements[:3]
+    article_urls = driver.find_elements(By.CLASS_NAME, "WlydOe")
+    return article_urls[:3]
 
 
 with open('summaries.txt', 'w') as f:     # clear the text file
     f.close()
 
 
-with open('summaries.txt', 'a') as f:
+with open('summaries.txt', 'a') as f:     # main
     for i in my_tickers:
+        driver.get(f'https://www.google.com/search?q={i}+stock&hl=en&tbm'
+                   f'=nws&source=lnt&tbs=sbd:1&sa=X&ved=2ahUKEwitoYTL16n9AhVNP'
+                   f'n0KHbyMBE4QpwV6BAgBECE&biw=2067&bih=2007&dpr=1')
         f.write(f'{i}\n')
-        data = scrape(i)
-        for element in data:
-            f.write(f'{get_title(element)}\n')
-            f.write(f'{get_date(element)}\n')
-            f.write(f'{get_url(element)}\n')
+        url_element = scrape_urls()
+        info_element = scrape_info()
+        for url, info in zip(url_element, info_element):
+            f.write(f'{get_title(info)}\n')
+            f.write(f'{get_date(info)}\n')
+            f.write(f'{get_url(url)}\n')
         f.write('\n\n')
-        #for url in urls:
-            #f.write(f'{get_info(url)}\n')
-            #f.write(f'{url}\n\n')
 f.close()
