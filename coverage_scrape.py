5bed882ad6ea9beb461462a8a71127569d20d8d0
nbradley7407 <121584105+nbradley7407@users.noreply.github.com>
2023-02-24 13:13:10 -0700

    refactoring

--- coverage_scrape.py
+++ coverage_scrape.py
@@ -56,41 +56,46 @@ def get_info(article):
         print(f"Error getting summary")
 
 
+def get_title(element):
+
+    title_element = element.find_element(By.XPATH, "//*[@id=\"rso\"]/div/div/div[1]/div/div/a/div/div[2]/div[2]")
+    title = title_element.get_attribute("innerHTML")
+    return title
+
+
+def get_url(element):
+    url = element.get_attribute('href')
+    return url
+
+
+def get_date(element):
+    date_element = element.find_element(By.XPATH, "//*[@id=\"rso\"]/div/div/div[1]/div/div/a/div/div[2]/div[4]")
+    date = date_element.get_attribute("innerHTML")
+    return date
+
+
 def scrape(ticker):
     driver.get(f'https://www.google.com/search?q={ticker}+stock&hl=en&tbm'
                f'=nws&source=lnt&tbs=sbd:1&sa=X&ved=2ahUKEwitoYTL16n9AhVNP'
                f'n0KHbyMBE4QpwV6BAgBECE&biw=2067&bih=2007&dpr=1')
     wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "WlydOe")))
     article_elements = driver.find_elements(By.CLASS_NAME, "WlydOe")
-    article_titles = []
-    article_dates = []
-    article_urls = []
-    for element in article_elements[:3]:
-        # get title
-        title_element = element.find_element(By.XPATH, "//*[@id=\"rso\"]/div/div/div[1]/div/div/a/div/div[2]/div[2]")
-        title = title_element.get_attribute("innerHTML")
-        article_titles.append(title)
-
-        # get date
-        date_element = element.find_element(By.TAG_NAME, "span")
-        date = date_element.get_attribute("span")
-        article_dates.append(date)
-
-        # get url
-        href = element.get_attribute('href')
-        article_urls.append(href)
-    return [article_titles, article_dates, article_urls]
+    return article_elements[:3]
 
 
 with open('summaries.txt', 'w') as f:     # clear the text file
     f.close()
 
 
-for i in my_tickers:
-    data = scrape(i)
-    with open('summaries.txt', 'a') as f:
+with open('summaries.txt', 'a') as f:
+    for i in my_tickers:
         f.write(f'{i}\n')
-        f.write(f'{data}')
+        data = scrape(i)
+        for element in data:
+            f.write(f'{get_title(element)}\n')
+            f.write(f'{get_date(element)}\n')
+            f.write(f'{get_url(element)}\n')
+        f.write('\n\n')
         #for url in urls:
             #f.write(f'{get_info(url)}\n')
             #f.write(f'{url}\n\n')
