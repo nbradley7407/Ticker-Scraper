d256f5a7146ca04b97f312640a86a5213d695595
nbradley7407 <121584105+nbradley7407@users.noreply.github.com>
2023-03-16 07:00:39 -0600

    bug fix
    title and blurb was returning the date. changed CSS selector

--- coverage_scrape.py
+++ coverage_scrape.py
@@ -39,13 +39,13 @@ def main():
                           f'=nws&source=lnt&tbs=sbd:1&sa=X&ved=2ahUKEwitoYTL16n9AhVNP'\
                           f'n0KHbyMBE4QpwV6BAgBECE&biw=2067&bih=2007&dpr=1'
             response = session.get(current_url)
-            f.write('<br><br>'+('_'*150))
+            f.write(f'<br><br>{"_" * 150}')
             f.write(f'<h1><p style="margin-left:50px";><a href="{current_url}"; style="color:green;">')
             f.write(f'{i}\n')
             f.write('</a></p></h1>')
             url_elements = response.html.find('.WlydOe')[:num_articles ]
-            title_elements = response.html.find('.nDgy9d')[:num_articles ]
-            blurb_elements = response.html.find('.nDgy9d')[:num_articles ]
+            title_elements = response.html.find('.mCBkyc.ynAwRc.MBeuO.nDgy9d')[:num_articles ]
+            blurb_elements = response.html.find('.mCBkyc.ynAwRc.MBeuO.nDgy9d')[:num_articles ]
             date_elements = response.html.find('.YsWzw')[:num_articles ]
             for url_element, title_element, blurb_element, date_element in zip(url_elements, title_elements, blurb_elements, date_elements):
                 f.write(f'<h3 style="margin-left:50px"><a href="')
